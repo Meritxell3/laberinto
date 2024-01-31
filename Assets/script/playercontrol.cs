53217f07f27b63c86b0fddf2a4e6373ac3cfd9d5
Meritxell3 <meritxelldiazramirez@gmail.com>
2024-01-31 19:51:42 +0100

    laberinto

---
+++ Assets/script/playercontrol.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontrol : MonoBehaviour
+{
+private Rigidbody playerb; //el rb del player
+private float speed = 0.5f; // velocidad
+private float speedJump = 50f; // velocidad de salto
+private Vector3 moveInput; // inputs de movimiento
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      playerb = GetComponent <Rigidbody>(); // le pedimos el rb
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveZ = Input.GetAxisRaw("Vertical");
+        moveInput = new Vector3(moveX,0,moveZ);
+
+        if(Input.GetKey("space") && playerb.velocity.y == 0){
+            playerb.AddForce(transform.up * speedJump);
+            Debug.Log("espacio,aprete espacio");
+        }
+
+    }
+private void FixedUpdate(){
+playerb.MovePosition(playerb.position + moveInput.normalized *speed* Time.fixedDeltaTime);// que el rb mueva la position del player sumandole el movimiento del imput por la velocidad por el tiempo que se ejecuta
+}
+
+
+
+
+}
